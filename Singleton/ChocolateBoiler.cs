5db3a2caa60168639933047a2a34612c89ab2fff
SergioGomezAlvarez <sgmichielr@gmail.com>
2025-10-06 14:18:08 +0200

    Add singleton with lock to Chocolateboiler, Ensured safety and create 1 instance

---
+++ Singleton/ChocolateBoiler.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Singleton
+{
+    internal class ChocolateBoiler
+    {
+        private bool empty;
+        private bool boiled;
+
+        private static ChocolateBoiler instance;
+
+        private static readonly object lockObject = new object();
+
+        public static ChocolateBoiler Instance
+        {
+            get
+            {
+                lock (lockObject)
+                {
+                    if (instance == null)
+                    {
+                        instance = new ChocolateBoiler();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        public bool IsEmpty { get { return this.empty; } }
+        public bool IsBoiled { get { return this.boiled; } }
+
+        private ChocolateBoiler()
+        {
+            empty = true;
+            boiled = false;
+        }
+
+        public void Fill()
+        {
+            if (empty)
+            {
+                empty = false;
+                boiled = false;
+                Console.WriteLine("Boiler filled with chocolate and milk mixture.");
+            }
+        }
+
+        public void Boil()
+        {
+            if (!empty && !boiled)
+            {
+                boiled = true;
+                Console.WriteLine("Boiler contents boiled.");
+            }
+        }
+
+        public void Drain()
+        {
+            if (!empty && boiled)
+            {
+                empty = true;
+                Console.WriteLine("Boiler drained.");
+            }
+        }
+    }
+}
